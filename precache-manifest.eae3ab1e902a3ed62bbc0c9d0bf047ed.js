106b2252afd998f48b070d63eeedbf736cc7d3dc
ChrisHadj <60141623+ChrisHadj@users.noreply.github.com>
2020-10-06 07:06:35 +0300

    Updates

---
+++ precache-manifest.eae3ab1e902a3ed62bbc0c9d0bf047ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f3c31ceeaf7cbd6d05211e8d98f8ec4",
+    "url": "/search-api/index.html"
+  },
+  {
+    "revision": "2a52ed76e148976d13d9",
+    "url": "/search-api/static/css/main.c00f04db.chunk.css"
+  },
+  {
+    "revision": "89e1240a068b56b52e81",
+    "url": "/search-api/static/js/2.201571fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/search-api/static/js/2.201571fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a52ed76e148976d13d9",
+    "url": "/search-api/static/js/main.d6731849.chunk.js"
+  },
+  {
+    "revision": "4ddb64d6e917481008f5",
+    "url": "/search-api/static/js/runtime-main.5f678042.js"
+  }
+]);
